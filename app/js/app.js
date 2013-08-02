99f3ba1334e708dadd6050f425b03841fff84bf8
makefu <github@syntax-fehler.de>
2013-08-03 01:48:29 +0200

    root -> static/ in angular

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
 
   angular.module('soundFlower', ['soundFlower.filters', 'soundFlower.services', 'soundFlower.directives', 'soundFlower.controllers']).
     config(['$routeProvider', function ($routeProvider) {
-      $routeProvider.when('/', {templateUrl: 'partials/channels.html', controller: 'ChannelsController'});
+      $routeProvider.when('/', {templateUrl: 'static/partials/channels.html', controller: 'ChannelsController'});
       $routeProvider.otherwise({redirectTo: '/'});
     }]);
 
