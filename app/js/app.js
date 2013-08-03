472805979929725df69ab4691d0f8c09ec1950af
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2013-08-03 02:46:42 +0200

    Replace mocks with real resources

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,13 @@
 (function () {
   'use strict';
 
-  angular.module('soundFlower', ['soundFlower.filters', 'soundFlower.services', 'soundFlower.directives', 'soundFlower.controllers']).
+  angular.module('soundFlower', [
+      'soundFlower.filters',
+      'soundFlower.services',
+      'soundFlower.directives',
+      'soundFlower.controllers',
+      'ngResource'
+    ]).
     config(['$routeProvider', function ($routeProvider) {
       $routeProvider.when('/', {templateUrl: 'static/partials/channels.html', controller: 'ChannelsController'});
       $routeProvider.otherwise({redirectTo: '/'});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,55 +4,29 @@
   angular.module('soundFlower.controllers', [])
 
     .controller('ChannelsController', [
-      '$scope',
-      function ($scope) {
+      '$scope', '$resource', 'channels', 'files',
+      function ($scope, channels, files) {
 
-        $scope.channels = [
-          {
-            id: 1,
-            state: 0,
-            file: null,
-            name: 'First channel'
-          },
-          {
-            id: 2,
-            state: 0,
-            file: 2,
-            name: 'First channel'
-          },
-          {
-            id: 3,
-            state: 1,
-            file: 1,
-            name: 'First channel'
-          }
-        ];
+        $scope.channels = channels.query();
 
-        $scope.files = [
-          {
-            id: 1,
-            name: 'sound1.mp3'
-          },
-          {
-            id: 2,
-            name: 'sound2.mp3'
-          },
-          {
-            id: 3,
-            name: 'sound3.mp3'
-          },
-          {
-            id: 4,
-            name: 'sound4.mp3'
-          }
-        ];
+        $scope.files = files.query();
+
 
         $scope.play = function (channel) {
           channel.state = 1;
+
+          channels.play({
+            channelId: 1,
+            fileId: channel.fileId
+          });
         };
 
         $scope.stop = function (channel) {
           channel.state = 0;
+
+          channels.stop({
+            channelId: 1
+          });
         };
 
       }
